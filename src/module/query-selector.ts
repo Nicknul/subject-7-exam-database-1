dc248a7fbc236d1c9992b092f8ab08b2628b904e
Nicknul <jea7730@gmail.com>
2024-07-09 18:07:20 +0900

    move_정호연 : src/module > static으로 이동

--- src/module/query-selector.ts
+++
@@ -1,2 +0,0 @@
-export const historyContainer = document.querySelector('.history');
-export const purechaseHistory = document.getElementById('purchase-history');
---
+++ static/query-selector.ts
@@ -0,0 +1,3 @@
+export const historyContainer = document.querySelector('.history');
+export const purechaseHistory = document.querySelector('#purchase-history');
+export const nowMoney = document.querySelector('#nowmoney');

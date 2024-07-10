e1bc117c284cef71eaf8b3a0cef424997b2f26f2
Nicknul <jea7730@gmail.com>
2024-07-10 21:39:24 +0900

    :egg: 정호연 : image name 추가

--- static/item.js
+++ static/item.js
@@ -3,14 +3,14 @@
  * * 상점 아이템 목록
  */
 export const item = [
-  { name: '어디로든 랜덤 문', price: 5000 },
-  { name: '정체불명의 알', price: 8000 },
-  { name: '요술 망치', price: 9900 },
-  { name: '수명', price: 10000 },
-  { name: '이상한 종이', price: 1000 },
-  { name: 'GPT 가방', price: 10000 },
-  { name: '기억의 알약', price: 2500 },
-  { name: '멀미 물약', price: 5000 },
-  { name: '푹잠 물약', price: 5000 },
-  { name: '신비의 돌', price: 7000 },
+  { name: '어디로든 랜덤 문', price: 5000, imageName: 'door' },
+  { name: '정체불명의 알', price: 8000, imageName: 'egg' },
+  { name: '요술 망치', price: 9900, imageName: 'hammer' },
+  { name: '수명', price: 10000, imageName: 'life' },
+  { name: '이상한 종이', price: 1000, imageName: 'paper' },
+  { name: 'GPT 가방', price: 10000, imageName: 'picnick' },
+  { name: '기억의 알약', price: 2500, imageName: 'pill' },
+  { name: '멀미 물약', price: 5000, imageName: 'sickness-potion' },
+  { name: '푹잠 물약', price: 5000, imageName: 'sleep-potion' },
+  { name: '신비의 돌', price: 7000, imageName: 'stone' },
 ];

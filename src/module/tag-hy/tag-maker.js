a72d94e20589344344cfcd81181c9f603510d748
Nicknul <jea7730@gmail.com>
2024-07-10 22:04:59 +0900

    :bulb: 정호연 : 주석 수정

--- src/module/tag-hy/tag-maker.js
+++ src/module/tag-hy/tag-maker.js
@@ -1,7 +1,7 @@
 /**
  * * 24.07.10 (Wed) 정호연
  * * 태그 생성 함수
- * @param {*} tagName 생성할 태그 이름
+ * @param {string} tagName 생성할 태그 이름
  * @returns 문서에 태그 생성
  */
 export const tagMaker = (tagName) => {

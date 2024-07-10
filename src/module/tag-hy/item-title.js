b1d329e003fc7738902c97617909d5f34415be4b
Nicknul <jea7730@gmail.com>
2024-07-10 23:19:52 +0900

    :bulb: 정호연 : 주석 수정

--- src/module/tag-hy/item-title.js
+++ src/module/tag-hy/item-title.js
@@ -3,7 +3,7 @@ import { tagMaker } from './tag-maker.js';
  * * 24.07.10 (wed) 정호연
  * * 아이템 제목
  * @param {object} itemName item.name
- * @returns
+ * @returns 아이템 이름이 들어간 div
  */
 export const itemTitle = (itemName) => {
   const title = tagMaker('div');

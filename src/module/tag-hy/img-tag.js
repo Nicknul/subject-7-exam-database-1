f7b2cfd2d48a6163b7f64129f6b0eaae82b584ef
Nicknul <jea7730@gmail.com>
2024-07-10 21:40:39 +0900

    :construction_worker: 정호연 : <div> 생성 확인

--- src/module/tag-hy/img-tag.js
+++ src/module/tag-hy/img-tag.js
@@ -1,11 +1,16 @@
 import { item } from '../../../static/item.js';
-import { createTags } from './create-tags.js';
+import { tagMaker } from './tag-maker.js';
 
 export const itemContainer = document.querySelector('.item-container');
+
 export const itemImg = () => {
   item.forEach((element) => {
-    console.log(element);
+    const title = tagMaker('div');
+    title.innerHTML = element.name;
+    title.className = 'item';
+    title.setAttribute('data-name', element.name);
+    console.log(title);
   });
 };
 
-itemContainer.appendChild(createTags('div'));
+itemContainer.appendChild(tagMaker('div'));

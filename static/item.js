079d03085d0b19a9e9c9c9eb8263d09fe5428d90
Nicknul <jea7730@gmail.com>
2024-07-10 20:28:23 +0900

    :rocket: 정호연 : item 목록

---
+++ static/item.js
@@ -0,0 +1,16 @@
+/**
+ * * 24.07.10 (Wed) 정호연
+ * * 상점 아이템 목록
+ */
+export const item = [
+  { name: '어디로든 랜덤 문', price: 5000 },
+  { name: '정체불명의 알', price: 8000 },
+  { name: '요술 망치', price: 9900 },
+  { name: '수명', price: 10000 },
+  { name: '이상한 종이', price: 1000 },
+  { name: 'GPT 가방', price: 10000 },
+  { name: '기억의 알약', price: 2500 },
+  { name: '멀미 물약', price: 5000 },
+  { name: '푹잠 물약', price: 5000 },
+  { name: '신비의 돌', price: 7000 },
+];

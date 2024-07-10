572a659b2f43c2203b4b4a4ec39fbbac50544a02
Nicknul <jea7730@gmail.com>
2024-07-10 23:37:56 +0900

    :pencil2: 정호연 : 매개변수 수정

--- src/module/tag-hy/item-title.js
+++ src/module/tag-hy/item-title.js
@@ -2,11 +2,11 @@ import { tagMaker } from './tag-maker.js';
 /**
  * * 24.07.10 (wed) 정호연
  * * 아이템 제목
- * @param {object} itemName item.name
+ * @param {object} element item.name
  * @returns 아이템 이름이 들어간 div
  */
-export const itemTitle = (itemName) => {
+export const itemTitle = (element) => {
   const title = tagMaker('div');
-  title.innerHTML = itemName;
+  title.innerHTML = element.name;
   return title;
 };

edefa785e0bde811ba59213b3401cd70bb021e3d
Nicknul <jea7730@gmail.com>
2024-07-10 22:07:06 +0900

    :rocket: 정호연 : class attribute

---
+++ src/module/attribute/class.js
@@ -0,0 +1,9 @@
+/**
+ * * 24.07.10 (Wed) 정호연
+ * * class attribute
+ * @param {element} element
+ * @param {string} classNames
+ */
+export const classAtt = (element, classNames) => {
+  element.className = classNames;
+};
